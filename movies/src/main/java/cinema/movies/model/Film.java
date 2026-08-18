3643df91538565f2b958ba2d96239e2108fd6b9f
marhilmi <marhilmi@192.168.11.57>
2026-08-18 11:48:30 +0100

    Model package - creating

---
+++ movies/src/main/java/cinema/movies/model/Film.java
@@ -0,0 +1,73 @@
+package cinema.movies.model;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Film extends AbstractModel<Long>{
+	
+	private static final long serialVersionUID = 2996009286487492970L;
+
+	@Column(nullable = false, length = 50)
+    private String titre;
+	
+	@Column(nullable = false)
+    private int duree;
+
+	@Column(nullable = false)
+    private int annee;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="GENRE_ID")
+    private Genre genre;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="NATIONALITE_ID")
+	@JsonProperty
+    private Nationalite nationalite;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="DIRECTOR_ID")
+    private Personne realisateur;
+
+    @ManyToMany
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JoinTable(
+      name="FILM_ACTEUR",
+      joinColumns=@JoinColumn(name="FILM_ID", referencedColumnName="ID"),
+      inverseJoinColumns=@JoinColumn(name="ACTEUR_ID", referencedColumnName="ID"))
+    private List<Personne> acteurs;
+    
+    @OneToMany(mappedBy = "film")
+    @JsonIgnore
+	private List<Seance> seances;
+    
+    @OneToMany(mappedBy = "film", cascade = {CascadeType.ALL}, orphanRemoval = true)
+    @JsonIgnore
+    @LazyCollection(LazyCollectionOption.FALSE)
+	private List<Media> medias;
+    
+    @Column(name = "added_date", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
+    private Date addedDate;  
+}
---
+++ movies/src/main/java/cinema/movies/model/Personne.java
@@ -0,0 +1,73 @@
+package cinema.movies.model;
+
+
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Personne extends AbstractModel<Long>{
+
+	private static final long serialVersionUID = -2974953413266908441L;
+
+	public enum TypePersonne {ACTEUR, REALISATEUR} 
+	
+	@Column(nullable = false, length = 50)
+    private String nom;
+
+    @Column(nullable = false, length = 50)
+    private String prenom;
+    
+    @Column(nullable = true, length = 100)
+    private String photo;
+    
+    @Column(name = "date_naissance")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date dateNaissance;
+    
+    @Column(nullable = false, length = 50)
+    @Enumerated(EnumType.STRING)
+    private TypePersonne typePersonne;
+
+    @Column(name = "added_date", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
+    private Date addedDate;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="NATIONALITE_ID")
+    private Nationalite nationalite;
+    
+    @ManyToMany(mappedBy="acteurs", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private List<Film> films;
+    
+    @OneToMany(mappedBy = "realisateur", fetch = FetchType.EAGER)
+    @JsonIgnore
+	private List<Film> filmsRealises; 
+    
+    
+    public String toString() {
+    	return this.prenom + " " + this.nom;
+    }
+}
